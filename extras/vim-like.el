f5e46e6db42f90d7526f5314c53651c9f0a2fd41
Rigoberto L. Perez <rlperez@kablamo.me>
2024-08-06 22:28:09 -0400

    feat: 🎸 add evil collections and leader

--- extras/vim-like.el
+++ extras/vim-like.el
@@ -17,16 +17,26 @@
 ;; Evil: vi emulation
 (use-package evil
   :ensure t
-
   :init
   (setq evil-respect-visual-line-mode t)
   (setq evil-undo-system 'undo-redo)
-
   ;; Enable this if you want C-u to scroll up, more like pure Vim
   ;(setq evil-want-C-u-scroll t)
-
   :config
   (evil-mode)
-
   ;; Configuring initial major mode for some modes
   (evil-set-initial-state 'vterm-mode 'emacs))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :init
+  (setq evil-collection-setup-minibuffer t)
+  :config
+  (evil-collection-init))
+
+(use-package evil-leader
+  :after evil
+  :ensure t
+  :config
+  (global-evil-leader-mode))
